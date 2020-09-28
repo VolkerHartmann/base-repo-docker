326dbc5f865b6d88ff9e18b2089835ffb06614b5
ThomasJejkal <thomas.jejkal@kit.edu>
2020-09-28 10:11:17 +0200

    Fixed wrong command for checking out branch

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git clone --recursive https://github.com/kit-data-manager/base-repo.git
 cd base-repo
 export LATEST_TAG=`git describe --abbrev=0 --tags`
 echo "Building latest release tagged as " $LATEST_TAG
-git checkout tags/$LATEST_TAG -b master
+git checkout tags/$LATEST_TAG
 
 echo "Building project from source."
 ./gradlew -Prelease build
